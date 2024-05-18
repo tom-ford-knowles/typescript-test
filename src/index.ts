d9f7f90d63fd20efc5d140e340d40a3252e2e7a3
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2024-05-18 13:57:40 +0100

    Remove package

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,57 @@
-// Please read the README for instructions on what to do.
-import { StatusCode } from "status-code-enum"
+enum HttpStatusCode {
+  ClientErrorBadRequest = 400,
+  ClientErrorConflict = 409,
+  ClientErrorExpectationFailed = 417,
+  ClientErrorFailedDependency = 424,
+  ClientErrorForbidden = 403,
+  ClientErrorGone = 410,
+  ClientErrorImATeapot = 418,
+  ClientErrorLengthRequired = 411,
+  ClientErrorLocked = 423,
+  ClientErrorLoginTimeOut = 440,
+  ClientErrorMethodNotAllowed = 405,
+  ClientErrorMisdirectedRequest = 421,
+  ClientErrorNotAcceptable = 406,
+  ClientErrorNotFound = 404,
+  ClientErrorPayloadTooLarge = 413,
+  ClientErrorPaymentRequired = 402,
+  ClientErrorPreconditionFailed = 412,
+  ClientErrorPreconditionRequired = 428,
+  ClientErrorProxyAuthRequired = 407,
+  ClientErrorRangeNotSatisfiable = 416,
+  ClientErrorRequestHeaderFieldsTooLarge = 431,
+  ClientErrorRequestTimeout = 408,
+  ClientErrorRetryWith = 449,
+  ClientErrorTooManyRequests = 429,
+  ClientErrorUnauthorized = 401,
+  ClientErrorUnavailableForLegalReasons = 451,
+  ClientErrorUnprocessableEntity = 422,
+  ClientErrorUnsupportedMediaType = 415,
+  ClientErrorUpgradeRequired = 426,
+  ClientErrorURITooLong = 414,
+  ServerErrorBadGateway = 502,
+  ServerErrorBandwidthLimitExceeded = 509,
+  ServerErrorGatewayTimeout = 504,
+  ServerErrorHTTPVersionNotSupported = 505,
+  ServerErrorInsufficientStorage = 507,
+  ServerErrorInternal = 500,
+  ServerErrorLoopDetected = 508,
+  ServerErrorNetworkAuthRequired = 511,
+  ServerErrorNotExtended = 510,
+  ServerErrorNotImplemented = 501,
+  ServerErrorServiceUnavailable = 503,
+  ServerErrorVariantAlsoNegotiates = 506,
+}
 
 type MethodConfig = {
   methodName: string
-  statusCode: StatusCode
+  statusCode: HttpStatusCode
   message: string
 }
 
 type HttpResponse = {
-  statusCode: StatusCode
-  message: string
+  readonly statusCode: HttpStatusCode
+  readonly message: string
 }
 
 function createErrorMethods(config: MethodConfig[]) {
@@ -17,8 +59,9 @@ function createErrorMethods(config: MethodConfig[]) {
     [key: string]: (messageOverride?: string) => HttpResponse
   } = {}
   config.forEach(configObject => {
-    const methodName = configObject.methodName
-    methods[methodName] = function (messageOverride?: string): HttpResponse {
+    methods[configObject.methodName] = function (
+      messageOverride?: string
+    ): HttpResponse {
       return {
         statusCode: configObject.statusCode,
         message: messageOverride ?? configObject.message,
@@ -32,13 +75,15 @@ function createErrorMethods(config: MethodConfig[]) {
 const errorMethods = createErrorMethods([
   {
     methodName: "badRequest",
-    statusCode: StatusCode.ClientErrorBadRequest,
+    statusCode: HttpStatusCode.ClientErrorBadRequest,
     message: "Bad request",
   },
   {
     methodName: "notFound",
-    statusCode: StatusCode.ClientErrorNotFound,
+    statusCode: HttpStatusCode.ClientErrorNotFound,
     message: "Not found",
   },
 ])
 const { badRequest, notFound } = errorMethods
+
+const statusCode = badRequest().statusCode
